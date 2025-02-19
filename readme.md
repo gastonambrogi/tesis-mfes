9d7d3f6556f6d4dcd6eb28fce8367e9498111481
Gaston Ambrogi <gaston.ambrogi@WS-005-C0DH9C0W6M.local>
2025-02-18 21:57:28 -0300

    Root Config added loader for headermfe

--- readme.md
+++ readme.md
@@ -16,3 +16,11 @@ yarn start
 yarn
 yarn start:standalone
 ```
+
+### CI/CD
+
+Eveything is connected in vercel to be deployed independently into its bucket/server, but escentially it runs on each directory
+
+```
+yarn build
+```
--- root-config/src/gatesis-root-config.js
+++ root-config/src/gatesis-root-config.js
@@ -6,14 +6,24 @@ import {
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
 
-const routes = constructRoutes(microfrontendLayout);
+const data = {
+  loaders: {
+    headerLoader: `<nav> loading </nav>`,
+  },
+};
+
+const routes = constructRoutes(microfrontendLayout, data);
+
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
     return import(/* webpackIgnore: true */ name);
   },
 });
-const layoutEngine = constructLayoutEngine({ routes, applications });
+const layoutEngine = constructLayoutEngine({
+  routes,
+  applications,
+});
 
 applications.forEach(registerApplication);
 layoutEngine.activate();
