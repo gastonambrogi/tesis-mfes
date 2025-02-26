a9f2fa1c9534c6ac7e0afc44928c324e0f38fb81
Gaston Ambrogi <gaston.ambrogi@WS-005-C0DH9C0W6M.local>
2025-02-26 20:24:23 -0300

    Created each mfe for the routes in header. Root Config linked to each new mfe

---
+++ about-mfe/src/root.component.tsx
@@ -0,0 +1,11 @@
+import { Content } from "@carbon/react";
+import "@carbon/styles/css/styles.css";
+
+export default function Root(props) {
+  return (
+    <Content>
+      <h1>Welcome to Our Website</h1>
+      <p>This is the main content area of the about module.</p>
+    </Content>
+  );
+}
